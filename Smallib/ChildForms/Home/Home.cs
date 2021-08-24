95fadd170cdc07aacc693bb6fda99625dd0d1905
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-08-23 21:53:51 -0300

    :construction: Home design screen

---
+++ Smallib/ChildForms/Home/Home.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smallib.ChildForms
+{
+    public partial class Home : Form
+    {
+        private Principal _principal;
+
+        public Home(Principal principal)
+        {
+            InitializeComponent();
+            _principal = principal;
+        }
+    }
+}
