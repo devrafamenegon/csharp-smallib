13a5f30a155fe78e8a5a14343f95e7442a6f1ceb
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-08-29 23:53:54 -0300

    :sparkles: Relatório Gênero

--- Smallib/ChildForms/Cadastros/Genero/GeneroMenu.Designer.cs
+++ Smallib/ChildForms/Cadastros/Genero/GeneroMenu.Designer.cs
@@ -29,9 +29,9 @@ namespace Smallib.ChildForms.Cadastros.Genero
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
             this.labelDgv = new System.Windows.Forms.Label();
             this.dgvGenero = new System.Windows.Forms.DataGridView();
             this.id_genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -49,6 +49,7 @@ namespace Smallib.ChildForms.Cadastros.Genero
             this.btnAtualizar = new System.Windows.Forms.Button();
             this.btnPesquisar = new System.Windows.Forms.Button();
             this.metroTxtBoxPesquisar = new MetroFramework.Controls.MetroTextBox();
+            this.btnRelatorio = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvGenero)).BeginInit();
             this.MainTopPanel.SuspendLayout();
             this.panel5.SuspendLayout();
@@ -71,40 +72,40 @@ namespace Smallib.ChildForms.Cadastros.Genero
             this.dgvGenero.AllowUserToResizeRows = false;
             this.dgvGenero.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvGenero.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(81)))), ((int)(((byte)(116)))));
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(81)))), ((int)(((byte)(116)))));
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvGenero.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(81)))), ((int)(((byte)(116)))));
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(81)))), ((int)(((byte)(116)))));
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvGenero.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
             this.dgvGenero.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvGenero.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.id_genero,
             this.nome_genero});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(81)))), ((int)(((byte)(116)))));
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(179)))), ((int)(((byte)(239)))));
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvGenero.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle11.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(81)))), ((int)(((byte)(116)))));
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(179)))), ((int)(((byte)(239)))));
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvGenero.DefaultCellStyle = dataGridViewCellStyle11;
             this.dgvGenero.EnableHeadersVisualStyles = false;
             this.dgvGenero.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
             this.dgvGenero.Location = new System.Drawing.Point(60, 177);
             this.dgvGenero.MultiSelect = false;
             this.dgvGenero.Name = "dgvGenero";
             this.dgvGenero.ReadOnly = true;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(81)))), ((int)(((byte)(116)))));
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(179)))), ((int)(((byte)(239)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvGenero.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(81)))), ((int)(((byte)(116)))));
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(179)))), ((int)(((byte)(239)))));
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvGenero.RowHeadersDefaultCellStyle = dataGridViewCellStyle12;
             this.dgvGenero.RowHeadersVisible = false;
             this.dgvGenero.RowTemplate.Height = 43;
             this.dgvGenero.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
@@ -203,7 +204,6 @@ namespace Smallib.ChildForms.Cadastros.Genero
             this.radioNomeGenero.Name = "radioNomeGenero";
             this.radioNomeGenero.Size = new System.Drawing.Size(68, 24);
             this.radioNomeGenero.TabIndex = 19;
-            this.radioNomeGenero.TabStop = true;
             this.radioNomeGenero.Text = "Nome";
             this.radioNomeGenero.UseVisualStyleBackColor = true;
             this.radioNomeGenero.CheckedChanged += new System.EventHandler(this.radioNomeGenero_CheckedChanged);
@@ -211,6 +211,7 @@ namespace Smallib.ChildForms.Cadastros.Genero
             // radioIdGenero
             // 
             this.radioIdGenero.AutoSize = true;
+            this.radioIdGenero.Checked = true;
             this.radioIdGenero.Cursor = System.Windows.Forms.Cursors.Hand;
             this.radioIdGenero.FlatAppearance.BorderSize = 2;
             this.radioIdGenero.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -257,6 +258,7 @@ namespace Smallib.ChildForms.Cadastros.Genero
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.panel5.Controls.Add(this.btnRelatorio);
             this.panel5.Controls.Add(this.labelDgv);
             this.panel5.Controls.Add(this.dgvGenero);
             this.panel5.Controls.Add(this.btnConsultar);
@@ -327,7 +329,6 @@ namespace Smallib.ChildForms.Cadastros.Genero
             this.metroTxtBoxPesquisar.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.metroTxtBoxPesquisar.CustomButton.UseSelectable = true;
             this.metroTxtBoxPesquisar.CustomButton.Visible = false;
-            this.metroTxtBoxPesquisar.Enabled = false;
             this.metroTxtBoxPesquisar.FontSize = MetroFramework.MetroTextBoxSize.Tall;
             this.metroTxtBoxPesquisar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(81)))), ((int)(((byte)(116)))));
             this.metroTxtBoxPesquisar.Lines = new string[0];
@@ -347,6 +348,22 @@ namespace Smallib.ChildForms.Cadastros.Genero
             this.metroTxtBoxPesquisar.WaterMark = "Pesquisar";
             this.metroTxtBoxPesquisar.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(197)))), ((int)(((byte)(197)))));
             this.metroTxtBoxPesquisar.WaterMarkFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btnRelatorio
+            // 
+            this.btnRelatorio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(129)))), ((int)(((byte)(233)))));
+            this.btnRelatorio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRelatorio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRelatorio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRelatorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRelatorio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.btnRelatorio.Location = new System.Drawing.Point(833, 552);
+            this.btnRelatorio.Name = "btnRelatorio";
+            this.btnRelatorio.Size = new System.Drawing.Size(110, 40);
+            this.btnRelatorio.TabIndex = 45;
+            this.btnRelatorio.Text = "Relatório";
+            this.btnRelatorio.UseVisualStyleBackColor = false;
+            this.btnRelatorio.Click += new System.EventHandler(this.btnRelatorio_Click);
             // 
             // GeneroMenu
             // 
@@ -387,5 +404,6 @@ namespace Smallib.ChildForms.Cadastros.Genero
         private MetroFramework.Controls.MetroTextBox metroTxtBoxPesquisar;
         private System.Windows.Forms.DataGridViewTextBoxColumn id_genero;
         private System.Windows.Forms.DataGridViewTextBoxColumn nome_genero;
+        private System.Windows.Forms.Button btnRelatorio;
     }
 }
--- Smallib/ChildForms/Cadastros/Genero/GeneroMenu.cs
+++ Smallib/ChildForms/Cadastros/Genero/GeneroMenu.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 
 namespace Smallib.ChildForms.Cadastros.Genero
@@ -63,11 +66,35 @@ namespace Smallib.ChildForms.Cadastros.Genero
         private void radioIdGenero_CheckedChanged(object sender, EventArgs e)
         {
             metroTxtBoxPesquisar.Enabled = true;
+
+            try
+            {
+                dados = new SqlDataAdapter("select pk_id_genero, nome_genero from Genero order by pk_id_genero asc", conectar);
+                datb = new DataTable();
+                dados.Fill(datb);
+                dgvGenero.DataSource = datb;
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possivel se conectar ao banco de dados!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
         private void radioNomeGenero_CheckedChanged(object sender, EventArgs e)
         {
             metroTxtBoxPesquisar.Enabled = true;
+
+            try
+            {
+                dados = new SqlDataAdapter("select pk_id_genero, nome_genero from Genero order by nome_genero", conectar);
+                datb = new DataTable();
+                dados.Fill(datb);
+                dgvGenero.DataSource = datb;
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possivel se conectar ao banco de dados!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
         private void btnPesquisar_Click(object sender, EventArgs e)
@@ -106,10 +133,9 @@ namespace Smallib.ChildForms.Cadastros.Genero
             dados.Fill(datb);
             dgvGenero.DataSource = datb;
 
-            radioIdGenero.Checked = false;
+            radioIdGenero.Checked = true;
             radioNomeGenero.Checked = false;
             metroTxtBoxPesquisar.Text = "";
-            metroTxtBoxPesquisar.Enabled = false;
 
             checarLinhasDgv();
         }
@@ -173,6 +199,89 @@ namespace Smallib.ChildForms.Cadastros.Genero
 
                 //Chama o forms de edição de gênero passando a instancia da classe com os valores da linha selecionada
                 _principal.OpenChildForm(new GeneroConsultar(_principal, cadastro_genero));
+            }
+        }
+
+        private void btnRelatorio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //verificando se a pasta pdf existe
+                string path = @"c:\pdf";
+                if (Directory.Exists(path) == false)
+                {
+                    //criando pasta pdf
+                    DirectoryInfo di = Directory.CreateDirectory(path);
+                }
+
+                //lugar onde o relatorio pfd vai ficar
+                string caminho = @"C:\pdf\" + DateTime.Now.ToString("yyyyMMdd") + "_RelatorioGenero_SmallLibrary.pdf";
+
+                //Criação do documento
+                Document doc = new Document(PageSize.A4);
+                PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(caminho, FileMode.Create));
+
+                /*
+                string caminho = System.Environment.CurrentDirectory; //cria o caminho com o PATH do diretório atual
+                string nomearquivo = caminho + @"\relatoriogenero.pdf"; //nome do documento e caminho (no momento, o PDF é criado dentro de bin\debug)
+                FileStream arquivoPDF = new FileStream(nomearquivo, FileMode.Create); //Diz o modo do arquivo
+                Document doc = new Document(PageSize.A4); //Criação do documento
+                PdfWriter escritorpdf = PdfWriter.GetInstance(doc, arquivoPDF); //Criação do escritor de pdf
+                */
+
+                // Parte dos paragrafos
+                string dados = "";
+
+                //small library
+                Paragraph smallibrary = new Paragraph(dados, new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 14, (int)System.Drawing.FontStyle.Bold)); //Criação do objeto paragrafo, com fonte normal, 14, em negrito
+                smallibrary.Alignment = Element.ALIGN_RIGHT; //Alinha o conteudo a direita
+                smallibrary.Add("\n SISTEMA SMALL LIBRARY");
+
+                //cabeçalho
+                Paragraph cabecalho = new Paragraph(dados, new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 14));
+                cabecalho.Alignment = Element.ALIGN_LEFT; //Alinha o conteudo a esquerda
+                cabecalho.Add("\n RELAÇÃO DE GÊNEROS DOS LIVROS");
+
+                //separador de linha
+                Paragraph l = new Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_LEFT, 1))); //Linha separadora
+
+                //Classificação
+                Paragraph classificacao = new Paragraph(dados, new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12));
+                classificacao.Alignment = Element.ALIGN_LEFT; //Alinha o conteudo a esquerda
+                classificacao.Add("Classificação: Identificação do Gênero \n\n");
+
+                //Tabela
+                PdfPTable table = new PdfPTable(2);
+                table.DefaultCell.FixedHeight = 20;
+                table.WidthPercentage = 100;
+
+                table.AddCell("ID do Gênero");
+                table.AddCell("Nome do Gênero");
+
+                for (int i = 0; i < (dgvGenero.Rows.Count); i++)
+                {
+                    table.AddCell(dgvGenero[0, i].Value.ToString());
+                    table.AddCell(dgvGenero[1, i].Value.ToString());
+                }
+
+                //Necessário abrir para poder inserir os elementos
+                doc.Open();
+                doc.Add(smallibrary);
+                doc.Add(cabecalho);
+                doc.Add(l);
+                doc.Add(classificacao);
+                doc.Add(table);
+                doc.Close();
+
+                MessageBox.Show("PDF gerado.");
+
+                //abrindo o pdf
+                System.Diagnostics.Process.Start(caminho);
+            }
+
+            catch
+            {
+                MessageBox.Show("Não foi possivel gerar o PDF", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
     }
