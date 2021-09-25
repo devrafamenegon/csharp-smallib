ce900f4092dce70c122c420cd0ec863287b9a803
C0bal <dev.rafaelmenegon@gmail.com>
2021-09-24 22:03:31 -0300

    :sparkles: Global variables

--- Smallib/Program.cs
+++ Smallib/Program.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Smallib
 {
     static class Program
     {
+        public static SqlConnection conectar = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog = BIBLIOTECA; Integrated Security = True");
+        public static SqlDataAdapter dados; //uma das classes que auxilia na recuperação de dados
+        public static SqlCommandBuilder cmd; //mostra os códigos SQL
+        public static DataTable datb; //DataTable é quem vai receber os dados do adapter
+
         /// <summary>
         /// Ponto de entrada principal para o aplicativo.
         /// </summary>
         [STAThread]
         static void Main()
-     {
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Senha()) ;
+            Application.Run(new Senha());
         }
+
     }
 }
