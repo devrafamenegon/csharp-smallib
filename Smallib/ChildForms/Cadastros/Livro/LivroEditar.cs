e5b5c8b98d84b8e1419ccf048aad564642d71449
C0bal <dev.rafaelmenegon@gmail.com>
2021-09-24 22:31:47 -0300

    :sparkles: Livro global variables

--- Smallib/ChildForms/Cadastros/Livro/LivroEditar.cs
+++ Smallib/ChildForms/Cadastros/Livro/LivroEditar.cs
@@ -9,16 +9,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Globalization;
+using static Smallib.Program;
 
 namespace Smallib.ChildForms.Cadastros.Livros
 {
     public partial class LivroEditar : Form
     {
-        SqlConnection conectar = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog = BIBLIOTECA; Integrated Security = True"); //Variável para conexão com o banco
-        SqlDataAdapter dados; //uma das classes que auxilia na recuperação de dados
-        SqlCommandBuilder cmd; //mostra os códigos SQL
-        DataTable datb; //DataTable é quem vai receber os dados do adapter
-
         string titulo_livro, tombo_livro;
         int pk_id_livro, fk_id_editora_livro, fk_id_genero_livro, fk_id_autor_livro, fk_id_idioma_livro;
 
--- Smallib/ChildForms/Cadastros/Livro/LivroMenu.cs
+++ Smallib/ChildForms/Cadastros/Livro/LivroMenu.cs
@@ -11,16 +11,12 @@ using System.Data.SqlClient;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.IO;
+using static Smallib.Program;
 
 namespace Smallib.ChildForms.Cadastros.Livros
 {
     public partial class LivroMenu : Form
     {
-        SqlConnection conectar = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog = BIBLIOTECA; Integrated Security = True");
-        SqlDataAdapter dados; //uma das classes que auxilia na recuperação de dados
-        SqlCommandBuilder cmd; //mostra os códigos SQL
-        DataTable datb; //DataTable é quem vai receber os dados do adapter
-
         //Criando um objeto da classe livro
         Cadastro_Livro livro = new Cadastro_Livro();
 
