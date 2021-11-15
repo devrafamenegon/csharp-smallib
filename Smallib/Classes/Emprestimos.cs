07c62ab0beb078ff5b7e643c788df86c0604d4e0
C0bal <dev.rafaelmenegon@gmail.com>
2021-11-15 12:01:28 -0300

    Fixing the loan structure

---
+++ Smallib/Classes/Emprestimos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smallib
+{
+    public class Emprestimo_Livro
+    {
+        public int pk_id_emprestimo { get; set; }
+        public string nome_leitor { get; set; }
+        public string titulo_livro { get; set; }
+        public DateTime dt_emprestimo { get; set; }
+        public DateTime dt_dev_prev_emprestimo { get; set; }
+        public DateTime dt_devolucao_emprestimo { get; set; }
+        public int status_emprestimo { get; set; }
+    }
+}
